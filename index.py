523567ba86d555cd0b96a197448b7c5134b76549
Suryakant Agarwal <suryaa62@gmail.com>
2022-08-31 16:20:20 +0530

    fix: deep link

--- index.py
+++ index.py
@@ -10,6 +10,10 @@ def index():
 def indexWithAmount(amount):
 	return flask.render_template("index.html" , amount=amount)
 
+@app.route("/test")
+def test():
+	return flask.redirect('upi://pay?pa=7828369492@ybl&amp;pn=Suryakant Agrawal&amp;cu=INR')
+
 if (__name__ == '__main__' ) :
 	app.run(debug=True)
 
